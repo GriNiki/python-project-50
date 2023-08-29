85782d0ea9403351e81205d7fc0d515e52a389f5
Nikita Grishin <mr_obey@mail.ru>
2023-08-29 22:04:48 +0300

    Vers 1.1.1 fix bugs

--- gendiff/generate_diff_tree.py
+++ gendiff/generate_diff_tree.py
@@ -6,37 +6,28 @@ def generate_diff_tree(dict1, dict2):
     for key in sorted_key:
 
         if key not in dict1:
-            diff_tree.append({
-                'value': key,
-                'item': dict2[key],
-                'status': 'added'
-                                })
+            diff_tree.append({'value': key,
+                              'item': dict2[key],
+                              'status': 'added'})
 
         elif key not in dict2:
-            diff_tree.append({
-                    'value': key,
-                    'item': dict1[key],
-                    'status': 'delete'
-                                })
+            diff_tree.append({'value': key,
+                              'item': dict1[key],
+                              'status': 'delete'})
 
         elif dict1[key] == dict2[key]:
-            diff_tree.append({
-                'value': key,
-                'item': dict1[key],
-                'status': 'no_change'
-                                })
+            diff_tree.append({'value': key,
+                              'item': dict1[key],
+                              'status': 'no_change'})
 
         elif isinstance(dict1[key], dict) and isinstance(dict2[key], dict):
-            diff_tree.append({
-                    'value': key,
-                    'children': generate_diff_tree(dict1[key], dict2[key]),
-                    'status': 'attachment'
-                                })
+            diff_tree.append({'value': key,
+                              'children':
+                                  generate_diff_tree(dict1[key], dict2[key]),
+                              'status': 'attachment'})
         else:
-            diff_tree.append({
-                    'value': key,
-                    'old_value': dict1[key],
-                    'new_value': dict2[key],
-                    'status': 'plain_changes'
-                                })
+            diff_tree.append({'value': key,
+                              'old_value': dict1[key],
+                              'new_value': dict2[key],
+                              'status': 'plain_changes'})
     return diff_tree
