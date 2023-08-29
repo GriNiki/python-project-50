94a61959af7e0d7562a2ad583129bb4de8c385a8
Nikita Grishin <mr_obey@mail.ru>
2023-08-29 21:53:39 +0300

    Vers 1.1.1 fix bugs and edit tests

--- testes/test_generate_diff.py
+++ testes/test_generate_diff.py
@@ -29,30 +29,30 @@ def open_file(path):
 
 def test_flat_json_file():
     assert (generate_diff(FILES['file1_json'], FILES['file2_json']) ==
-            open_file(FILES['expected_result_json_flat']))
+        open_file(FILES['expected_result_json_flat']))
 
 
 def test_flat_yaml_file():
     assert (generate_diff(FILES['file1_yaml'], FILES['file2_yaml']) ==
-            open_file(FILES['expected_result_yaml_flat']))
+        open_file(FILES['expected_result_yaml_flat']))
 
 
 def test_stylish_format():
     assert (generate_diff(FILES['file1_tree_json'], FILES['file2_tree_json'], 'stylish') ==
-            open_file(FILES['expected_result_tree']))
+        open_file(FILES['expected_result_tree']))
     assert (generate_diff(FILES['file1_tree_yaml'], FILES['file2_tree_yaml'], 'stylish') ==
-            open_file(FILES['expected_result_tree']))
+        open_file(FILES['expected_result_tree']))
 
 
 def test_plain_format():
     assert (generate_diff(FILES['file1_tree_json'], FILES['file2_tree_json'], 'plain') ==
-            open_file(FILES['expected_result_plain']))
+        open_file(FILES['expected_result_plain']))
     assert (generate_diff(FILES['file1_tree_yaml'], FILES['file2_tree_yaml'], 'plain') ==
-            open_file(FILES['expected_result_plain']))
+        open_file(FILES['expected_result_plain']))
 
 
 def test_json_format():
     assert (generate_diff(FILES['file1_tree_json'], FILES['file2_tree_json'], 'json') ==
-            open_file(FILES['expected_result_json']))
+        open_file(FILES['expected_result_json']))
     assert (generate_diff(FILES['file1_tree_yaml'], FILES['file2_tree_yaml'], 'json') ==
-            open_file(FILES['expected_result_json']))
+        open_file(FILES['expected_result_json']))
